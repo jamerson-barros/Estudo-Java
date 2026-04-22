9c128946423aa98466d6c0a7920394b534522521
Jamerson <jamersonbarrosdasilva@gmail.com>
2026-04-22 01:43:30 -0400

    Adcionei valor na variavel age

--- JavaDataType/src/TiposDeDados.java
+++ JavaDataType/src/TiposDeDados.java
@@ -7,8 +7,8 @@ public class TiposDeDados {
         */
 
         // INTEIROS
-        byte; // 8 bits (-128 - 127)
-        short; // 16 bits (-32.768 a 32.767)
+        byte age = 21; // 8 bits (-128 - 127)
+        short ; // 16 bits (-32.768 a 32.767)
         int; // 32 bits (-2.147.483.647 - 2.147.483.647) - Mais usado
         long; //64 bits (A cima do valor de int)
 
