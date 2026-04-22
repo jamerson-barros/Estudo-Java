8248d1607fda38a635ac62cd560dde5842bdf94a
Jamerson <jamersonbarrosdasilva@gmail.com>
2026-04-22 00:56:14 -0400

    Adcionando pastas no repositorio

---
+++ JavaDataType/src/TiposDeDados.java
@@ -0,0 +1,24 @@
+public class TiposDeDados {
+
+    public static void main(String[] args) {
+
+        /* TIPOS PRIMITIVOS:
+            São os tipos básicos da memória. Armazena os valores diretmente na memória.
+        */
+
+        // INTEIROS
+        byte; // 8 bits (-128 - 127)
+        short; // 16 bits (-32.768 a 32.767)
+        int; // 32 bits (-2.147.483.647 - 2.147.483.647) - Mais usado
+        long; //64 bits (A cima do valor de int)
+
+        // DECIMAIS (PONTO FLUTUANTES)
+        float; // 32 bits
+        double; // 64 bits - Mais usado
+
+
+
+
+
+    }
+}
