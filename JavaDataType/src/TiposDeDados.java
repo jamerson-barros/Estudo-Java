519c2f19f9b5d56e1636bf21b31719155eee89b2
Jamerson <jamersonbarrosdasilva@gmail.com>
2026-04-22 04:54:04 -0400

    adcionei mais variaveis

--- JavaDataType/src/TiposDeDados.java
+++ JavaDataType/src/TiposDeDados.java
@@ -2,17 +2,18 @@ public class TiposDeDados {
 
     public static void main(String[] args) {
 
-        /* TIPOS PRIMITIVOS:
-            São os tipos básicos da memória. Armazena os valores diretmente na memória.
-        */
+        /*
+        TIPOS PRIMITIVOS:
+            São os tipos basic da memória. Armazena os values retirement na memória.
+         */
 
-        // INTEIROS
+        // INTEROPS
         byte age = 21; // 8 bits (-128 - 127)
-        short ; // 16 bits (-32.768 a 32.767)
-        int; // 32 bits (-2.147.483.647 - 2.147.483.647) - Mais usado
-        long; //64 bits (A cima do valor de int)
+        short house_number = 1087; // 16 bits (-32.768 a 32.767)
+        int id = 01; // 32 bits (-2.147.483.647 - 2.147.483.647) - Mais usado
+        long person_in_the_world = 83000000000L; // 64 bits (A cima do valor de int) prefixo L
 
-        // DECIMAIS (PONTO FLUTUANTES)
+        // DECIMALS (PONTO FLUCTUATES)
         float; // 32 bits
         double; // 64 bits - Mais usado
 
