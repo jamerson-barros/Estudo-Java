84d714bb400867d9b161a0180fa716878f045343
Jamerson <jamersonbarrosdasilva@gmail.com>
2026-04-22 06:37:01 -0400

    aprendi usar System.out.printf(); e especificadores de formato para inteiros e decimais

--- JavaDataType/src/TiposDeDados.java
+++ JavaDataType/src/TiposDeDados.java
@@ -7,19 +7,21 @@ public class TiposDeDados {
             São os tipos basic da memória. Armazena os values retirement na memória.
          */
 
-        // INTEROPS
+        // INTEIROS
         byte age = 21; // 8 bits (-128 - 127)
         short house_number = 1087; // 16 bits (-32.768 a 32.767)
-        int id = 01; // 32 bits (-2.147.483.647 - 2.147.483.647) - Mais usado
+        int id = 1; // 32 bits (-2.147.483.647 - 2.147.483.647) - Mais usado
         long person_in_the_world = 83000000000L; // 64 bits (A cima do valor de int) prefixo L
 
-        // DECIMALS (PONTO FLUCTUATES)
-        float; // 32 bits
-        double; // 64 bits - Mais usado
+        // DECIMALS (PONTO FLUTUANTE)
+        float height = 1.86f; // 32 bits
+        double salary = 3500; // 64 bits - Mais usado
 
-
-
-
+        System.out.printf("%-20s %s%n", "", "Exemplos de Inteiros");
+        System.out.printf("%-15s: %d%n", "Idade", age);
+        System.out.printf("%-15s: %d%n", "Numero da casa", house_number);
+        System.out.printf("%-15s: %d%n", "ID", id);
+        System.out.printf("%-15s: %d%n", "Pessoas no Mundo", person_in_the_world);
 
     }
 }
